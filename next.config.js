6a4edbdfe70dd3cb5b7ba9657742610b82fa7c6e
Bertil Chapuis <bchapuis@gmail.com>
2023-03-02 00:56:26 +0100

    Add serve package

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  staticImage: true,
-  distDir: 'build',
-  reactStrictMode: true
-})
+  staticImage: true
+});
 
-module.exports = withNextra()
+module.exports = {
+  ...withNextra(),
+  images: {
+    unoptimized: true
+  }
+  //reactStrictMode: true
+};
