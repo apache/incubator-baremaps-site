721eb55ca39fdaa100600ab17ab026302631d698
Leonard <73912641+leonardcser@users.noreply.github.com>
2023-04-28 16:52:28 +0200

    Add support for favicon bookmarks (#19)

--- theme.config.tsx
+++ theme.config.tsx
@@ -29,6 +29,14 @@ const config: DocsThemeConfig = {
   },
   head: (
     <>
+      {/* Favicons generated with https://realfavicongenerator.net  */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
+      <meta name="msapplication-TileColor" content="#ffffff"/>
+      <meta name="theme-color" content="#ffffff"/>
       <meta name="ahrefs-site-verification" content="ef8280f8481d5ba95a69da0c39d5a06efcc8b8d55820581438c243e12b613846" />
     </>
   ),
