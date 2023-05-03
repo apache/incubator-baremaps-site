c0085e4e3838877fce4baaf4319514e702cc7a1a
Leonard <73912641+leonardcser@users.noreply.github.com>
2023-05-03 18:22:42 +0200

    Improve repository organization (#22)
    * Add tsconfig src path
    
    * Move libraries to dist folder
    
    * Add prettier
    
    * Update doc for prettier
    
    * Format code
    
    * Add cicd for prettier

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,47 +1,51 @@
 import React, { useRef, useEffect, useState } from 'react';
 import maplibregl from 'maplibre-gl';
-import MaplibreInspect from './maplibre-gl-inspect.js';
-import MaplibreTileBoundaries from './maplibre-gl-tile-boundaries.js';
+import MaplibreInspect from '@/lib/maplibre/dist/maplibre-gl-inspect/maplibre-gl-inspect';
+import MaplibreTileBoundaries from '@/lib/maplibre/dist/maplibre-gl-tile-boundaries/maplibre-gl-tile-boundaries';
 
-import styles from './style.module.css'
+import styles from './style.module.css';
 
 export default function Map() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng] = useState(6.625);
-    const [lat] = useState(46.510);
-    const [zoom] = useState(14);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng] = useState(6.625);
+  const [lat] = useState(46.51);
+  const [zoom] = useState(14);
 
-    useEffect(() => {
-        if (map.current) return;
-        map.current = new maplibregl.Map({
-            container: mapContainer.current,
-            style: `https://demo.baremaps.com/style.json`,
-            center: [lng, lat],
-            zoom: zoom
-        });
-        map.current.addControl(new maplibregl.NavigationControl({}));
-
-        // Add the inspect control to the map.
-        map.current.addControl(new MaplibreInspect({
-            showMapPopup: true,
-            showMapPopupOnHover: false,
-            showInspectMapPopupOnHover: false,
-            popup: new maplibregl.Popup({
-                closeButton: true,
-                closeOnClick: true
-            })
-        }));
-
-        // Add the tile boundaries control to the map.
-        map.current.addControl(new MaplibreTileBoundaries({
-            showBoundaries: false
-        }));
+  useEffect(() => {
+    if (map.current) return;
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://demo.baremaps.com/style.json`,
+      center: [lng, lat],
+      zoom: zoom
     });
+    map.current.addControl(new maplibregl.NavigationControl({}));
 
-    return (
-        <div className={styles.wrap}>
-            <div ref={mapContainer} className={styles.map} />
-        </div>
+    // Add the inspect control to the map.
+    map.current.addControl(
+      new MaplibreInspect({
+        showMapPopup: true,
+        showMapPopupOnHover: false,
+        showInspectMapPopupOnHover: false,
+        popup: new maplibregl.Popup({
+          closeButton: true,
+          closeOnClick: true
+        })
+      })
     );
+
+    // Add the tile boundaries control to the map.
+    map.current.addControl(
+      new MaplibreTileBoundaries({
+        showBoundaries: false
+      })
+    );
+  });
+
+  return (
+    <div className={styles.wrap}>
+      <div ref={mapContainer} className={styles.map} />
+    </div>
+  );
 }
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,45 +1,67 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMap } from '@fortawesome/free-regular-svg-icons'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMap } from '@fortawesome/free-regular-svg-icons';
+
 const config: DocsThemeConfig = {
   logo: (
     <>
-      <FontAwesomeIcon style={{ height: 26}} icon={faMap} />
-      <span style={{ marginLeft: '.4rem', fontWeight: 800, fontSize: 16 }}> Apache Baremaps (incubating)</span>
+      <FontAwesomeIcon style={{ height: 26 }} icon={faMap} />
+      <span style={{ marginLeft: '.4rem', fontWeight: 800, fontSize: 16 }}>
+        Apache Baremaps (incubating)
+      </span>
     </>
   ),
   project: {
-    link: 'https://github.com/apache/incubator-baremaps/',
+    link: 'https://github.com/apache/incubator-baremaps/'
   },
   feedback: {
     useLink: () => 'https://github.com/apache/incubator-baremaps/issues/new'
   },
-  docsRepositoryBase: 'https://github.com/apache/incubator-baremaps-site/blob/main/',
+  docsRepositoryBase:
+    'https://github.com/apache/incubator-baremaps-site/blob/main/',
   footer: {
-    text: 'Copyright © ' + new Date().getFullYear() + ' Apache Software Foundation',
+    text:
+      'Copyright © ' + new Date().getFullYear() + ' Apache Software Foundation'
   },
   sidebar: {
     defaultMenuCollapseLevel: 1
   },
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – Apache Baremaps'
-    }
+      titleTemplate: '%s - Apache Baremaps'
+    };
   },
   head: (
     <>
       {/* Favicons generated with https://realfavicongenerator.net  */}
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-      <link rel="manifest" href="/site.webmanifest"/>
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
-      <meta name="msapplication-TileColor" content="#ffffff"/>
-      <meta name="theme-color" content="#ffffff"/>
-      <meta name="ahrefs-site-verification" content="ef8280f8481d5ba95a69da0c39d5a06efcc8b8d55820581438c243e12b613846" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta
+        name="ahrefs-site-verification"
+        content="ef8280f8481d5ba95a69da0c39d5a06efcc8b8d55820581438c243e12b613846"
+      />
     </>
-  ),
-}
+  )
+};
 
-export default config
+export default config;
